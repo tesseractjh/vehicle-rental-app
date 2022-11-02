752a434d7dd714074732642be2d80258c08d8dd5
Tesseract <tensai3355@gmail.com>
2022-11-02 17:13:29 +0900

    Modify: mixin
    flex mixin에서 gap을 문자열로 받도록 변경

--- src/styles/theme/mixin.ts
+++ src/styles/theme/mixin.ts
@@ -1,5 +1,6 @@
-export const flex = (justifyContent = 'center', alignItems = 'center') => `
+export const flex = (justifyContent = 'center', alignItems = 'center', gap = '0') => `
   display: flex;
   justify-content: ${justifyContent};
   align-items: ${alignItems};
+  gap: ${gap};
 `;
